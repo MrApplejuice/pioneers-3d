bff3c6208950a4c1ccfc95fb3edc6c0dd828ac9b
Paul Konstantin Gerke <paulkgerke@craftware.info>
2019-09-21 21:10:08 +0200

    Add missing packages

--- .circleci/install-requirements.sh
+++ .circleci/install-requirements.sh
@@ -2,7 +2,9 @@
 
 set -e
 
-apt-get update > /dev/null &
+apt-get update
+
+apt-get install -y wget tar
 
 wget -O /tmp/cmake.tgz https://github.com/Kitware/CMake/releases/download/v3.15.3/cmake-3.15.3-Linux-x86_64.tar.gz
 tar -x -C /opt/ -f /tmp/cmake.tgz
@@ -19,8 +21,6 @@ mv /opt/cmake-* /opt/cmake
 #  wget -O /tmp/glib.tar.xz http://ftp.gnome.org/pub/gnome/sources/glib/2.60/glib-2.60.7.tar.xz ;
 #  tar -x -C $HOME/deps -f /tmp/glib.tar.xz 
 #) &
-
-wait
 
 # Build requirements
 apt-get install -y intltool libgtk2.0-dev libgtk-3-dev libnotify-dev yelp-tools
