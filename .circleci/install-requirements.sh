cefbd668cd871d989fdd2f5357b86cf666eea250
Paul Konstantin Gerke <paulkgerke@craftware.info>
2019-09-21 20:49:11 +0200

    Add gtk build files

--- .circleci/install-requirements.sh
+++ .circleci/install-requirements.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+set -e
+
 apt-get update > /dev/null &
 
 wget -O /tmp/cmake.tgz https://github.com/Kitware/CMake/releases/download/v3.15.3/cmake-3.15.3-Linux-x86_64.tar.gz
 tar -x -C /opt/ -f /tmp/cmake.tgz
 mv /opt/cmake-* /opt/cmake
+
+wget -O /tmp/gtk.tar.xz http://ftp.gnome.org/pub/gnome/sources/gtk+/3.24/gtk+-3.24.11.tar.xz
+mkdir deps
+tar -x -C deps -f /tmp/gtk.tar.xz
 
 # cmake in: /opt/cmake/bin/
 
